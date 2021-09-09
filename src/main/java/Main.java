7f66dc1757d43cbed05e5436f3c6d507e4b1a928
AntonDavydov <daver1107@yahoo.com>
2021-09-09 11:15:00 -0700

    Main class refactoring

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        World world = new World();
-        world.start();
+        new World().start();
     }
 }
